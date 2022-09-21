c96cda59d9c660b8101b3947826f415b8a3bf52d
Bluestonex63 <103859845+Bluestonex63@users.noreply.github.com>
2022-09-21 18:27:27 +0100

    Added levels and changed the title

--- script.js
+++ script.js
@@ -13,15 +13,59 @@ button.addEventListener("click", async () => {
 			let cats = await fetch(src + `games/${r.data.id}/categories`).then(x => x.json()).then(x => x.data)
 			let vari = await fetch(src + `games/${r.data.id}/variables`).then(x => x.json()).then(x => x.data)
 			let form = document.createElement("form")
+            form.id = "submissionform"
 			if (cats.length != 0) { 
 				let catlb = document.createElement("label")
-				catlb.innerHTML = "<label>Category:</label>"
+				catlb.innerHTML = "<label>Category: </label>"
 				let catsel = document.createElement("select")
-				cats.forEach(cat => {let opt = document.createElement("option"); opt.innerHTML = `<option>${cat["name"]}</option>`; catsel.appendChild(opt)})
+                catsel.id = "catsel"
+				cats.forEach(cat => {
+                    let opt = document.createElement("option");
+                    if (cat["type"] == "per-game") {
+                        opt.innerHTML = `<option>${cat["name"]} - FG</option>`
+                    } else {
+                        opt.innerHTML = `<option>${cat["name"]} - IL</option>`
+                    }
+                    catsel.appendChild(opt)
+                })
 				form.appendChild(catlb)
 				form.appendChild(catsel)
-				document.body.appendChild(form)
+                document.querySelector("#submission").innerHTML = ""
+				document.querySelector("#submission").appendChild(form)
 			} 
+            let lvlc = (lvls) => {
+                let form = document.querySelector("#submissionform")
+                if (lvls.length != 0 && document.querySelector("#catsel").value.split(" - ").splice(document.querySelector("#catsel").value.split(" - ").length-1).includes("IL")) {
+                    let lvllb = document.createElement("label")
+                    lvllb.innerHTML = "<label>Levels: </label>"
+                    let lvlsel = document.createElement("select")
+                    lvls.forEach(lvl => {
+                        let opt = document.createElement("option");
+                        opt.innerHTML = `<option>${lvl["name"]}</option>`
+                        lvlsel.appendChild(opt)
+                    })
+                    const catsel = document.querySelector("#catsel")
+                    form.innerHTML = ""
+                    let catlb = document.createElement("label")
+				    catlb.innerHTML = "<label>Category: </label>"
+                    form.appendChild(catlb)
+                    form.appendChild(catsel)
+                    form.appendChild(document.createElement("p"))
+                    form.appendChild(lvllb)
+                    form.appendChild(lvlsel)
+                    document.querySelector("#submission").innerHTML = ""
+                    document.querySelector("#submission").appendChild(form)
+                } else {
+                    const catsel = document.querySelector("#catsel")
+                    document.querySelector("#submissionform").innerHTML = ""
+                    let catlb = document.createElement("label")
+				    catlb.innerHTML = "<label>Category: </label>"
+                    document.querySelector("#submissionform").appendChild(catlb)
+                    document.querySelector("#submissionform").appendChild(catsel)
+                }
+            }
+            lvlc(lvls)
+            document.querySelector("#catsel").addEventListener("change", () => {lvlc(lvls)})
 		}
 		catch (e) {alert(`There was an error, please try again later. \n\n${e}`); button.disabled = false; throw e}
 	}
